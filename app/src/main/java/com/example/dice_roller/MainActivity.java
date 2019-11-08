28e708556931a5745d8fc49735f6f60c974b04cc
1715605 <56392739+1715605@users.noreply.github.com>
2019-11-08 00:03:40 +0000

    Randomly generated question

--- app/src/main/java/com/example/dice_roller/MainActivity.java
+++ app/src/main/java/com/example/dice_roller/MainActivity.java
@@ -13,6 +13,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private int counter = 0;
     private TextView countering;
     private TextView counterDP;
+    private TextView questionlist;
 
 
     @Override
@@ -39,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         userEntered = (EditText) findViewById(R.id.userEntered);
         countering = (TextView) findViewById(R.id.countering);
         counterDP = (TextView) findViewById(R.id.counterView);
+        questionlist = (TextView) this.findViewById(R.id.list);
 
 
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -76,6 +79,7 @@ public class MainActivity extends AppCompatActivity {
     public void on_button_click(View view) {
         TextView tv = this.findViewById(R.id.myTextView);
 
+
         Random r = new Random();
         int number = r.nextInt(7);
 
@@ -94,5 +98,26 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
+    }
+
+    public void randomlist(View view){
+
+        ArrayList<String> questionSelection = new ArrayList<>();
+        questionSelection.add("1) If you could go anywhere in the world, where would you go?");
+        questionSelection.add("2) If you were stranded on a desert island, what three things would you want to take with you?");
+        questionSelection.add("3) If you could eat only one food for the rest of your life, what would that be?");
+        questionSelection.add("4) If you won a million dollars, what is the first thing you would buy?");
+        questionSelection.add("5) If you could spaned the day with one fictional character, who would it be?");
+        questionSelection.add("6) If you found a magic lantern and a genie gave you three wishes, what would you wish?");
+
+        Random r = new Random();
+        int numElement = 1;
+
+        for (int i = 0; i < numElement; i++) {
+
+            int index = r.nextInt(questionSelection.size());
+            String randomE =  questionSelection.get(index);
+            questionlist.setText(randomE);
+        }
     }
 }
